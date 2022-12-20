b69ad50c22199ae002d337be503aa0aee26aac9f
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-20 10:12:20 +0300

    Add Browser Router

--- src/components/OurPartners.js
+++ src/components/OurPartners.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const OurPartners = () => {
   return (
@@ -13,14 +14,15 @@ const OurPartners = () => {
       </div>
         <div className="row">
             <div className="col-12 col-md-6 col-lg-4">
+                <Link>
                 <div className="card">
                     <img src="https://via.placeholder.com/300x200" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
+                </Link>
             </div>
         </div>
     </div>
