71f6dd56184472394f9ee80cd21a93f408ac67f3
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-20 10:32:02 +0300

    Create Homepage and NavBar files

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function NavBar () {
+
+
+    return(
+       
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+                    AfyaNet</a>
+                    <a>About Us</a>
+                    <a>Services</a>
+                    <a>Contact Us</a>
+                    <a>Our Partners</a>
+            </div>
+        </nav>
+    )
+}
+export default NavBar;
