7a1049232a71b74d7a976211d0d65701049b9aca
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-20 18:01:03 +0300

    Style navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import OurPartners from './components/OurPartners';
+import NavBar from './components/navbar/NavBar';
 
 
 function App() {
   return (
    
     <div className="App">
+      <NavBar/>
       <OurPartners />
     </div>
   );
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -7,16 +7,18 @@ function NavBar () {
 
     return(
        
+        
         <nav className="navbar navbar-light bg-light">
             <div class="container-fluid">
                 <div id="contents">
-                    <a class="navbar-brand" href="" style={{color: "592C7B"}}>
-                    <img src="./assests/Logo.png" alt=""  class="d-inline-block align-text-top"/>
-                    AfyaNet</a>
-                    <a href = "" style={{color: "black"}}>About Us</a>
-                    <a href = "">Services</a>
-                    <a href = "">Contact Us</a>
-                    <a href = "">Our Partners</a>
+                    {/* <a class="navbar-brand" href=" " style={{color: "#592C7B"}}> */}
+                    <img src=" " alt=""></img>
+                   <h3 style={{color: "#592C7B"}}>AfyaNet</h3> 
+                    {/* </a> */}
+                    <a href = " " style={{color: "black"}}>About Us</a>
+                    <a href = " " style={{color: "black"}}>Services</a>
+                    <a href = " " style={{color: "black"}}>Contact Us</a>
+                    <a href = " " style={{color: "black"}}>Our Partners</a>
                     </div>
             </div>
         </nav>
