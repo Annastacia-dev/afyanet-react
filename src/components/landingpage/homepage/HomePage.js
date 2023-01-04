a07aa72c71c020f6cd6c82ebc1abb36df890e930
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-04 10:30:04 +0300

    fix homepage responsiveness

--- src/components/landingpage/homepage/HomePage.js
+++ src/components/landingpage/homepage/HomePage.js
@@ -1,26 +1,31 @@
 import React from "react";
+import { Container,Row, Col, Button } from 'react-bootstrap';
+import image from "../../../assets/images/landing-page/doctor.jpg"
 import "./HomePage.css"
 
 function HomePage() {
 
     return (
-        <div className="homepage container">
-            <div>
-            <h3 class="text-start">Connecting patients and providers<br></br>
-            for better healthcare.</h3>
-            <p class="text-start">
-                Welcome to Afyanet, a platform for connecting patients<br></br>
-                with healthcare providers.Find and connect with the <br></br>
-                right healthcare provider, so you can get the care you need<br></br> 
-                when you need it.<br></br>
-                <br></br>
-                <button >Get Started</button>
-            </p>
-            </div>
-            <img id="doctor" src="https://img.freepik.com/free-photo/woman-doctor-wearing-lab-coat-with-stethoscope-isolated_1303-29791.jpg?w=2000" alt= "doc1"></img>
-            {/* <img  id ="doctor" src="./assets/images/landing-page/doctor.jpg" alt="doc"></img> */}
-        
+        <>
+        <div className="home">
+            <Container>
+                <Row>
+                    <Col md={6} className="home-content">
+                        <h3 className="home-title">
+                        Connecting patients and providers for better healthcare.
+                        </h3>
+                        <p className="home-text">
+                        Welcome to Afyanet, a platform for connecting patients with healthcare providers.Find and connect with the right healthcare provider, so you can get the care you need when you need it.
+                        </p>
+                        <Button variant="outline-success">Get Started</Button>
+                    </Col>
+                    <Col md={6} className="home-image">
+                        <img src={image} alt = "doctor" className="doctor" style={{width: "100%"}}/>
+                    </Col>
+                </Row>
+            </Container>
         </div>
+        </>
 
     )
 }
