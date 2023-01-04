9b48ed4a06ef82cc967dfde43d676e49fb2174f2
Andreen-Muchiri <andreenshii@gmail.com>
2023-01-04 10:48:24 +0300

    Add paragraph to about component

--- src/components/landingpage/about/About.jsx
+++ src/components/landingpage/about/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <div className='about us'>
+    <h1><strong>About Us</strong></h1>
+    <p class="right">
+      At Afyanet, our mission is to make it easy for patients to find and connect with the right healthcare providers.We understand that navigating the healthcare system can be overwhelming,which is why we've developed a platform that makes it simple and convenient for patients to access the care they need.
+    </p>
+    </div>
+   </>
   )
 }
 
