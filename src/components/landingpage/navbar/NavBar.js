451e16b3647b4bc9e00b311e663f98f26d98d382
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-04 10:20:26 +0300

    fix navbar responsiveness

--- src/components/landingpage/navbar/NavBar.js
+++ src/components/landingpage/navbar/NavBar.js
@@ -1,26 +1,40 @@
 import React from "react";
+import { Container,Navbar, Nav, Button } from 'react-bootstrap';
+import logo from "../../../assets/images/logo/afyanet.png";
 import "./NavBar.css"
 
 
 function NavBar () {
 
     return(
+        <>
+        <Navbar bg="light" expand="lg" className="home navbar navbar-expand-lg navbar-light fixed-top py-3">
+            <Container className="container px-4 px-lg-5">
+                <Navbar.Brand href="#home">
+                    <img src={logo} alt = "logo" className="logo" style={{width: "50px"}}/>
+                    <span className="navbar-brand">AfyaNet</span>
+                </Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                    {/* align center */}
+                    <Nav className="navbar-nav ms-auto my-2 my-lg-2 me-lg-4">
+                    <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                        <li className="nav-item"><Nav.Link href="#about">About Us</Nav.Link></li> 
+                        <li className="nav-item"><Nav.Link href="#services">Services</Nav.Link></li>
+                        <li className="nav-item"><Nav.Link href="#ourpartners">Our Partners</Nav.Link></li>
+                        <li className="nav-item"><Nav.Link href="#contact">Contact Us</Nav.Link></li>
+                    </ul>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggler navbar-toggler-left" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav className="navbar-nav ms-auto my-2 my-lg-0">
+                     <Button variant="outline-success">Log In</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </>
        
-        
-        <nav className="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <div id="contents">
-                    {/* <a class="navbar-brand" href=" " style={{color: "#592C7B"}}> */}
-                    <img src=" " alt=""></img>
-                   <h3 style={{color: "#592C7B"}}>AfyaNet</h3> 
-                    {/* </a> */}
-                    <a href = " ">About Us</a>
-                    <a href = " ">Services</a>
-                    <a href = " ">Contact Us</a>
-                    <a href = " ">Our Partners</a>
-                    </div>
-            </div>
-        </nav>
     )
 }
 export default NavBar;
