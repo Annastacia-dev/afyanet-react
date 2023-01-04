89aa24630a399e08974f22079ebd63b11b96bdf9
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-04 12:43:14 +0300

    patient sign up styling

--- src/components/authforms/patient/PatientSignUp.js
+++ src/components/authforms/patient/PatientSignUp.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Container,Row, Col, Form, Button, Navbar } from 'react-bootstrap';
+import { Container,Row, Col, Form, Button } from 'react-bootstrap';
 import BrandLogo from '../../logo/BrandLogo';
+import '../../../css/PatientSignUp.css'
 
 const PatientSignUp = () => {
+
   return (
     <div className="patient-signup">
-        <Navbar expand="lg" className="home navbar navbar-expand-lg fixed-top py-3">
-            <Container className="container px-4 px-lg-5">
-                <BrandLogo />
-            </Container>
-        </Navbar>
+         <BrandLogo />
         <Container className="container px-4 px-lg-5">
             <Row className="justify-content-center">
                 <Col lg="8">
-                    <h2 className="text-center mt-0">Get the care you deserve, SignUp As A Patient</h2>
+                    <h2 className="text-center">Get the care you deserve</h2>
                 </Col>
             </Row>
             <Row className="justify-content-center">
@@ -22,13 +20,11 @@ const PatientSignUp = () => {
                         <Row className="justify-content-center">
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicFirstName">
-                                    <Form.Label>First Name</Form.Label>
-                                    <Form.Control name="firstName" type="text" placeholder="Enter First Name*" required />
+                                    <Form.Control name="firstName" type="text" placeholder='Enter First Name*' required /> 
                                 </Form.Group>
                             </Col>
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicLastName">
-                                    <Form.Label>Last Name</Form.Label>
                                     <Form.Control name="lastName" type="text" placeholder="Enter Last Name*" required />
                                 </Form.Group>
                             </Col>
@@ -36,13 +32,12 @@ const PatientSignUp = () => {
                         <Row className="justify-content-center">
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
                                     <Form.Control name="email" type="email" placeholder="Enter Your Email*" required />
+                                    
                                 </Form.Group>
                             </Col>
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicPhone">
-                                    <Form.Label>Phone Number</Form.Label>
                                     <Form.Control name="phone" type="text" placeholder="Enter Phone Number*" required />
                                 </Form.Group>
                             </Col>
@@ -50,13 +45,11 @@ const PatientSignUp = () => {
                         <Row className="justify-content-center">
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicLocation">
-                                    <Form.Label>Location</Form.Label>
                                     <Form.Control name="location" type="text" placeholder="Enter Your Location*" required />
                                 </Form.Group>
                             </Col>
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicDateOfBirth">
-                                    <Form.Label>Date Of Birth</Form.Label>
                                     <Form.Control name="dateOfBirth" type="date" placeholder="Enter Your Date Of Birth*" required />
                                 </Form.Group>
                             </Col>
@@ -64,20 +57,18 @@ const PatientSignUp = () => {
                         <Row className="justify-content-center">
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
                                     <Form.Control name="password" type="password" placeholder="Enter Password*" required />
                                 </Form.Group>
                             </Col>
                             <Col lg="6">
                                 <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-                                    <Form.Label>Confirm Password</Form.Label>
                                     <Form.Control name="confirmPassword" type="password" placeholder="Confirm Password*" required />
                                 </Form.Group>
                             </Col>
                         </Row>
                         <Row className="justify-content-center">
                             <Col lg="6">
-                                <Button variant="primary" type="submit"> Submit </Button>
+                                <Button variant="primary" type="submit"> Sign Up </Button>
                             </Col>
                         </Row>
                     </Form>
@@ -86,7 +77,7 @@ const PatientSignUp = () => {
             <Row>
                 <Col lg="12">
                     <p className="text-center mt-0">Already have an account? 
-                    <Button variant='link' href="/patient-login">Login</Button></p>
+                    <Button type='submit' variant='link' href="/patient-login">Login</Button></p>
                 </Col>
             </Row>
             <Row>
@@ -97,8 +88,8 @@ const PatientSignUp = () => {
             </Row>
             <Row>
                 <Col lg="12">
-                    <p className="text-center mt-0">
                     <hr />
+                    <p className="text-center mt-0">
                     By Signing up you agree to our terms  & conditions and Our Privacy policy
                     </p>
                 </Col>
--- src/components/logo/BrandLogo.js
+++ src/components/logo/BrandLogo.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import logo from "../../assets/images/logo/afyanet.png";
 
 const BrandLogo = () => {
   return (
-    <div>
-        <Navbar.Brand href="#home">
-                    <img src={logo} alt = "logo" className="logo" style={{width: "50px"}}/>
-                    <span className="navbar-brand">AfyaNet</span>
-        </Navbar.Brand>
-    </div>
+      <Navbar
+      style={{
+        backgroundColor: "#eee7f6",
+        height: "60px"
+      }}
+      expand="lg" className="home navbar navbar-expand-lg fixed-top py-3">
+            <Container className="container px-4 px-lg-5">
+              <Navbar.Brand style={{display: "flex",alignItems: "center",justifyContent: "center"}} href="#home">
+                  <img src={logo} alt = "logo" className="logo" style={{width: "40px"}}/>
+                  <span className="navbar-brand" style={{color:"#592c7b",fontSize: "20px",fontWeight: "500",marginLeft: "10px"}}>AfyaNet</span>
+              </Navbar.Brand>
+            </Container>
+      </Navbar>
+    
   )
 }
 
