51af1f8fa86ec9eed234be171c22880fbb524af9
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-05 20:52:16 +0300

    patient login jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingpage/LandingPage';
 import PatientSignUp from './components/authforms/patient/PatientSignUp';
+import PatientLogIn from './components/authforms/patient/PatientLogIn';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/patient-signup" element={<PatientSignUp />} />
+        <Route path="/patient-login" element={<PatientLogIn />} />
       </Routes>
     </div>
   );
