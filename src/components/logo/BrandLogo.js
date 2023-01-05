aa9eaee09b436b5d3739e32c228455ced5c92955
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-05 12:56:10 +0300

    add brand logo media breakpoint margin size

--- src/components/logo/BrandLogo.js
+++ src/components/logo/BrandLogo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar, Container } from 'react-bootstrap';
 import logo from "../../assets/images/logo/afyanet.png";
+import './BrandLogo.css'
 
 const BrandLogo = () => {
   return (
@@ -11,7 +12,7 @@ const BrandLogo = () => {
       }}
       expand="lg" className="home navbar navbar-expand-lg fixed-top py-3">
             <Container className="container px-4 px-lg-5">
-              <Navbar.Brand style={{display: "flex",alignItems: "center",justifyContent: "center"}} href="#home">
+              <Navbar.Brand className='brandlogo' style={{display: "flex",alignItems: "center",justifyContent: "center"}} href="#home">
                   <img src={logo} alt = "logo" className="logo" style={{width: "40px"}}/>
                   <span className="navbar-brand" style={{color:"#592c7b",fontSize: "20px",fontWeight: "500",marginLeft: "10px"}}>AfyaNet</span>
               </Navbar.Brand>
