6c8f0e0a7ed52374dd411402e7989f47070493aa
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-09 21:03:49 +0300

    configure doctor route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React ,{useState,useEffect}from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingpage/LandingPage';
 import PatientSignUp from './components/authforms/patient/PatientSignUp';
@@ -9,46 +9,14 @@ import PatientAppointments from './components/dashboards/patient/PatientAppointm
 import PatientMedicalRecords from './components/dashboards/patient/PatientMedicalRecords';
 import PatientMessages from './components/dashboards/patient/PatientMessages';
 import PatientProfile from './components/dashboards/patient/PatientProfile';
-import DoctorSideBar from './components/dashboards/doctor/DoctorSideBar';
 import DoctorSignUp from './components/authforms/doctor/DoctorSignUp';
 import DoctorLogIn from './components/authforms/doctor/DoctorLogIn';
 import DoctorAuthenticate from './components/authforms/doctor/DoctorAuthenticate';
+import DoctorDashboard from './components/dashboards/doctor/DoctorDashboard';
 
 
 
 function App() {
-
-  // Patient
-
-  const [patient, setPatient] = useState([]);
-
-  // Doctor
-  const [doctor, setDoctor] = useState([]);
-
-  // get Patient
-  async function getPatient() {
-    const response = await fetch('http://localhost:3000/patient_profile')
-    const data = await response.json();
-    setPatient(data)
-  }
-
-  useEffect (() => {
-    getPatient()
-  },[])
-
-   // get Doctor
-   async function getDoctor() {
-    const response = await fetch('http://localhost:3000/doctor_profile')
-    const data = await response.json();
-    setDoctor(data)
-  }
-
-  useEffect (() => {
-    getDoctor()
-  },[])
-
-
-
 
 
   return (
@@ -56,12 +24,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/patient/signup" element={<PatientSignUp setPatient={setPatient} />} />
-        <Route path="/patient/login" element={<PatientLogIn setPatient={setPatient} />} />
-        <Route path="/patient/dashboard" element={<PatientDashboard patient={patient} />} />
-        <Route path="/doctor/signup" element={<DoctorSignUp setDoctor={setDoctor} />} />
-        <Route path="/doctor/login" element={<DoctorLogIn setDoctor={doctor} />} />
-        <Route path="/doctor/authenticate" element={<DoctorAuthenticate setDoctor={setDoctor} />} />
         <Route path="/patient/signup" element={<PatientSignUp/>} />
         <Route path="/patient/login" element={<PatientLogIn/>} />
         <Route path="/patient/dashboard" element={<PatientDashboard />} />
@@ -69,7 +31,10 @@ function App() {
         <Route path="/patient/medicalrecords" element={<PatientMedicalRecords />} />
         <Route path="/patient/messages" element={<PatientMessages />} />
         <Route path="/patient/profile" element={<PatientProfile />} />
-        <Route path="/doctor/sidebar" element={<DoctorSideBar />} />
+        <Route path="/doctor/signup" element={<DoctorSignUp  />} />
+        <Route path="/doctor/login" element={<DoctorLogIn />} />
+        <Route path="/doctor/authenticate" element={<DoctorAuthenticate  />} />
+        <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
       </Routes>
     </div>
   );
