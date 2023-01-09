1dccb0c2cc11148e16a6e6d6655f808eb15a8c78
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2023-01-09 13:44:07 +0300

    Add doctor signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LandingPage from './components/landingpage/LandingPage';
 import PatientSignUp from './components/authforms/patient/PatientSignUp';
 import PatientLogIn from './components/authforms/patient/PatientLogIn';
 import PatientDashboard from './components/dashboards/PatientDashboard';
+import DoctorSignUp from './components/authforms/doctor/DoctorSignUp';
+import DoctorLogIn from './components/authforms/doctor/DoctorLogIn';
 
 
 function App() {
@@ -12,6 +14,9 @@ function App() {
   // Patient
 
   const [patient, setPatient] = useState([]);
+
+  // Doctor
+  const [doctor, setDoctor] = useState([]);
 
   // get Patient
   async function getPatient() {
@@ -24,6 +29,16 @@ function App() {
     getPatient()
   },[])
 
+   // get Doctor
+   async function getDoctor() {
+    const response = await fetch('http://localhost:3000/doctor_profile')
+    const data = await response.json();
+    setDoctor(data)
+  }
+
+  useEffect (() => {
+    getDoctor()
+  },[])
 
 
 
@@ -37,6 +52,8 @@ function App() {
         <Route path="/patient/signup" element={<PatientSignUp setPatient={setPatient} />} />
         <Route path="/patient/login" element={<PatientLogIn setPatient={setPatient} />} />
         <Route path="/patient/dashboard" element={<PatientDashboard patient={patient} />} />
+        <Route path="/doctor/signup" element={<DoctorSignUp setDoctor={setDoctor} />} />
+        <Route path="/doctor/login" element={<DoctorLogIn setDoctor={doctor} />} />
       </Routes>
     </div>
   );
--- src/data/homepage.js
+++
@@ -1,8 +0,0 @@
-import doctor from '../assets/images/landing-page/doctor.jpg'
-
-export const doc = [
-    {
-        id: 1,
-        image: doctor
-    }
-]
