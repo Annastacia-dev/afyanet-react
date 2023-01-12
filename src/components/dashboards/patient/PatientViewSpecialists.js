6af48c4f67510bd152eb8043b1f06a593c765276
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-12 16:08:48 +0300

    patient book appointment

--- src/components/dashboards/patient/PatientViewSpecialists.js
+++ src/components/dashboards/patient/PatientViewSpecialists.js
@@ -13,36 +13,35 @@ const PatientViewSpecialists = () => {
 
   const { patient, specialists } = useContext(PatientContext)
 
-  const specialist = specialists.find(specialist => specialist.id === parseInt(id))
+  const specialist = specialists && specialists.find(specialist => specialist && specialist.id === parseInt(id))
 
-
-
+  const doctors = specialist && specialist.doctors
 
 
   return (
     <Container className="mt-5" fluid>
       <Row>
         <Col md={6}>
-          <h1>{specialist.name}s</h1>
+          <h1>{specialist && specialist.name}s</h1>
         </Col>
         </Row>
       <Row>
         {
-          specialist.doctors.length > 0 ? specialist.doctors.map(doctor => {
+           doctors ? doctors.length > 0 ? doctors.map(doctor => {
             return (
-              <Col key={doctor.id} md={4} className="mb-4">
-                <Card className="card">
+              <Col key={doctor.id} md={4} className="mt-5">
+                <Card>
                   <Card.Body>
-                    <Card.Title className="card-title">{doctor.first_name}</Card.Title>
-                    <Button 
-                    onClick={() => navigate(`/patient/bookappointment/${doctor.id}`)}
-                    variant="primary">Book Appointment</Button>
+                    <Card.Title>{doctor.first_name} {doctor.last_name}</Card.Title>
+                    <Card.Text>
+                      {doctor.bio}
+                    </Card.Text>
+                    <Button onClick={() => navigate(`/patient/bookappointment/${doctor.id}`)}>Book Appointment</Button>
                   </Card.Body>
                 </Card>
               </Col>
             )
-          }
-          ) : <h1>No doctors found</h1>
+          }) : <h1>No doctors found</h1> : <h1>Loading...</h1>
         }
       </Row>
       
