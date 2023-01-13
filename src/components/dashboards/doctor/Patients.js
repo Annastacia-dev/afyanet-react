8a932553bae356a6479d4eae41268b37f2f08507
Andreen-Muchiri <andreenshii@gmail.com>
2023-01-13 09:43:37 +0300

    Updates

--- src/components/dashboards/doctor/Patients.js
+++ src/components/dashboards/doctor/Patients.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React,  { useContext } from "react";
 import DoctorSideBar from './DoctorSideBar';
 import {Container,Row,Col,Card} from 'react-bootstrap';
 import "../../../css/DoctorPatients.css";
+import {  DoctorContext } from '../../../context/doctor';
 
 
 
 
 function Patients(){
 
+ 
+  const { doctor } = useContext(DoctorContext);
+  console.log(doctor)
+
     return(
      <>
+           <DoctorSideBar />
+
       <div className="d-flex" id="wrapper">
       <DoctorSideBar />
         <div id="page-content-wrapper">
           <div className="container-fluid sidebarcontentcontainer">
+           <Card className="first-patient">
            
-           <div class="col-sm-3">
-           
-         </div>
+          <Card.Title >doctors.first_na</Card.Title>
+             <Card.Text >
+             <i class="fa-solid fa-location-dot"></i>
+              Karen
+              </Card.Text>
+             <Card.Text >
+             <i class="fa-solid fa-cake-candles"></i>
+              35
+              </Card.Text>
+
+
+           </Card>
+      
         </div>
         </div>
         </div>
+
+
         </>
     )
 }
