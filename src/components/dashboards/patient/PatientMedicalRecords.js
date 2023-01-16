d73ecf9d417c7cb7cee18924912cc9f33afa4b7c
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-16 08:57:20 +0300

    patient medical records

--- src/components/dashboards/patient/PatientMedicalRecords.js
+++ src/components/dashboards/patient/PatientMedicalRecords.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import PatientSidebar from './PatientSidebar'
+import {Row, Col, Table} from 'react-bootstrap'
+import { PatientContext } from '../../../context/patient';
 
 const PatientMedicalRecords = () => {
+
+  const { patient } = useContext(PatientContext);
+
+
   return (
     <div className="d-flex" id="wrapper">
       <PatientSidebar />
         <div id="page-content-wrapper">
-          <div className="container-fluid">
-            <h1 className="mt-4">Medical Records</h1>
+          <div className="container-fluid sidecontentcontainer">
+            <Row className="sidecontent px-5 mt-5">
+              <Col md={12} className="mb-4">
+                <p>Health Data</p>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Blood Pressure</th>
+                      <th>Weight</th>
+                      <th>Height</th>
+                      <th>Temperature</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      {
+                        patient && patient.medical_records && patient.medical_records.map((record, index) => {
+                          return (
+                            <tr key={index}>
+                              <td>{record.blood_pressure ? record.blood_pressure : '0' }</td>
+                              <td>{record.weight ? record.weight : '0' }</td>
+                              <td>{record.height ? record.height : '0' }</td>
+                              <td>{record.temperature ? record.temperature : '0' }</td>
+                            </tr>
+                          )
+                        })
+                      }
+                  </tbody>
+                </Table>
+              </Col>
+              <Col md={12} className="mb-4">
+                <p>Allergies</p>
+                 
+              </Col>
+            </Row>
           </div>
         </div>
       
