e394be970c9cb6e318233d23442aa80f71c4d593
Andreen-Muchiri <andreenshii@gmail.com>
2023-01-16 22:25:06 +0300

    Changes in patient details page

--- src/components/dashboards/doctor/PatientDetails.js
+++ src/components/dashboards/doctor/PatientDetails.js
@@ -20,6 +20,9 @@ function PatientDetails(){
    const patient = patients && patients.find(patient => patient && patient.id === parseInt(id))
  console.log(patient)
 
+ 
+  
+  
 
     return(
         <>
@@ -82,7 +85,94 @@ function PatientDetails(){
                   
                   </Col>
 
-                  </Card>          
+                  <Row className="medi-records">
+                      <Col>
+                      <Card className="health-data">
+                       <Card.Body>
+                        <Card.Title className="health-title">
+                          Health Data
+                        </Card.Title>
+                        <Row className=" mt-5 sidecontent">
+                          <Col md={10}>
+                         <Table striped bordered hover className="healthdata-table">
+                         <thead>
+                      <tr className="appointment-subtitles">
+                       <th>Blood Pressure</th>
+                        <th >Weight</th>
+                        <th >Temperature</th>
+
+                       </tr>
+                    </thead>
+                    {patient && patient.medical_record && patient.medical_record.length > 0 && patient.medical_record.map((medical_record) =>{
+                      
+                       return(
+                        <tbody key={medical_record.id}>
+                        <tr>
+                          <td>{medical_record.blood_pressure}</td>
+                          <td>{medical_record.weight}</td>
+                          <td>{medical_record.temperature}</td>
+
+                             </tr>
+                      </tbody>
+                    )
+                    })}
+                  
+                         </Table>
+                          </Col>
+                        </Row>
+
+                       </Card.Body>
+                      </Card>
+                      </Col>
+
+                      {/* medication */}
+                      <Col className="medication">
+                         <Card className="medication-card">
+                          <Card.Body>
+                            <Card.Title className="medication-title">
+                              Medications
+                            </Card.Title>
+                           
+                            <ul class="list-group list-group-flush">
+                              { patient && patient.medical_record && patient.medical_record.length > 0 && patient.medical_record.map((medical_record, index)=>{
+                                return (
+                        <li key={index} class="list-group-item"
+>                          {medical_record.medications}
+                        </li>
+                                )
+                              })}
+
+                            </ul>
+                            </Card.Body>
+                          </Card>
+                      </Col>
+
+                      {/* allergies */}
+                      <Col className="allergies">
+                         <Card className="allergies-card">
+                          <Card.Body>
+                            <Card.Title className="allergies-title">
+                              Allergies
+                            </Card.Title>
+                           
+                            <ul className="list-group list-group-flush allergies-ul">
+                              { patient && patient.medical_record && patient.medical_record.length > 0 && patient.medical_record.map((medical_record, index)=>{
+                                return (
+                        <li key={index} class="list-group-item"
+>                          {medical_record.allergies}
+                        </li>
+                                )
+                              })}
+
+                            </ul>
+                            </Card.Body>
+                          </Card>
+                      </Col>
+
+                    </Row>      
+                  </Card>   
+
+                 
       <Container>
                   {/* <Row>
                {patient && patient.appointments && patient.appointments.length > 0 && patient.appointments.map((appointment) =>
