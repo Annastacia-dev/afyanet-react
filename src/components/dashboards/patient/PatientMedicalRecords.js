87be3634d8a9a7440371d1a8bd6a4c322f58c992
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-17 13:54:06 +0300

    medical-records responsiveness

--- src/components/dashboards/patient/PatientMedicalRecords.js
+++ src/components/dashboards/patient/PatientMedicalRecords.js
@@ -1,6 +1,6 @@
 import React,{ useContext } from 'react'
 import PatientSidebar from './PatientSidebar'
-import {Row, Col, Table} from 'react-bootstrap'
+import {Row, Col, Table, Card} from 'react-bootstrap'
 import { PatientContext } from '../../../context/patient';
 import '../../../css/PatientMedicalRecords.css'
 import AddAllergies from './AddAllergies';
@@ -21,9 +21,10 @@ const PatientMedicalRecords = () => {
 
 
   return (
+    <>
     <div className="patient-medical-records d-flex" id="wrapper">
       <PatientSidebar />
-        <div id="page-content-wrapper">
+        <div className='medical-records-desktop' id="page-content-wrapper">
           <div className="container-fluid sidecontentcontainer">
             <Row className="sidecontent px-5 mt-5">
               <Col md={12} className="health-data mb-4">
@@ -146,8 +147,148 @@ const PatientMedicalRecords = () => {
             </Row>
           </div>
         </div>
+
+        {/* Mobile Phone */}
+
+        <div className='medical-records-phone' id="page-content-wrapper">
+          <div className="container-fluid sidecontentcontainer">
+            <Row className="sidecontent px-5 mt-5">
+              <Col md={12} className="health-data mb-4">
+                <p className='heading'>Health Data
+                {
+                          patient && patient.medical_record && patient.medical_record.blood_pressure && patient.medical_record.weight && patient.medical_record.height && patient.medical_record.temperature ? (
+                            <Popup trigger={<button className='healthdata-edit'><i className="fa-solid fa-edit"></i></button>}
+                            modal
+                            nested
+                            >
+                              <EditHealthData/>
+                            </Popup>
+                          ) : (
+                            <Popup trigger={<button className='healthdata-add'><i className="fa-solid fa-plus"></i>Add</button>}
+                            modal
+                            nested
+                            >
+                              <AddHealthData/>
+                            </Popup>
+                          )
+
+                }
+                </p>
+                
+                <Card>
+                     {
+                      <>
+                        <p>Blood Pressure:
+                          <br/>
+                          {patient && patient.medical_record && patient.medical_record.blood_pressure} mmHg</p>
+                        <p>Weight:
+                          <br/>
+                          {patient && patient.medical_record && patient.medical_record.weight} kgs</p>
+                        <p>Height:
+                          <br/>
+                          {patient && patient.medical_record && patient.medical_record.height} ft</p>
+                        <p>Temperature:
+                          <br/>
+                          {patient && patient.medical_record && patient.medical_record.temperature}&deg;C</p>
+                      </>
+                     }
+                </Card>
+              </Col>
+              <Row>
+              <Col md={12} className="allergies mb-4">
+                <p>Allergies</p>
+                {
+                  // if patient has no allergies, render add allergies button,else edit allergies button
+                  patient && patient.medical_record && patient.medical_record.allergies && patient.medical_record.allergies.length > 0 ? (
+                    <Popup trigger={<button className="btn btn-primary"><i className="fa-solid fa-pen"></i>Edit</button>}
+                    modal
+                    nested
+                    >
+                      <EditAllergies/>
+                    </Popup>
+                  ) : (
+                    <Popup trigger={<button className="btn btn-primary"><i className="fa-solid fa-plus"></i>Add</button>} 
+                    modal
+                    nested
+                    >
+                     <AddAllergies/> 
+                    </Popup>
+                  )
+                }
+                {
+                  // if patient has allergies, convert the string to list
+
+                  patient && patient.medical_record && patient.medical_record.allergies && patient.medical_record.allergies.length > 0 ? (
+                    <ul>
+                      {
+                        patient.medical_record.allergies.split(',').map((allergy, index) => (
+                          <li key={index}><i className="fa-solid fa-angle-right"></i>{allergy}</li>
+                        ))
+                      }
+                    </ul>
+                  ) : (
+                    <h5>No allergies</h5>
+                  )
+                }   
+              </Col>
+              </Row>
+              <Row>
+              <Col md={12} className="patient-records-medications mb-4">
+                <p>Medications</p>
+                {
+                  // if patient has no medications, render add medications button,else edit medications button
+                  patient && patient.medical_record && patient.medical_record.medications && patient.medical_record.medications.length > 0 ? (
+                    <Popup trigger={<button className="btn btn-primary"><i className="fa-solid fa-pen"></i>Edit</button>}
+                    modal
+                    nested
+                    >
+                      <EditMedications/>
+                    </Popup>
+                    
+                  ) : (
+                    <Popup trigger={<button className="btn btn-primary"><i className="fa-solid fa-plus"></i>Add</button>}
+                    modal
+                    nested
+                    >
+                      <AddMedications/>
+                    </Popup>
+                  )
+                }
+                {
+                  // if patient has medications, convert the string to list
+
+                  patient && patient.medical_record && patient.medical_record.medications && patient.medical_record.medications.length > 0 ? (
+                    <ul>
+                      {
+                        patient.medical_record.medications.split(',').map((medication, index) => (
+                          <li key={index}><i className="fa-solid fa-angle-right"></i> {medication}</li>
+                        ))
+                      }
+                    </ul>
+                  ) : (
+                    <h5>No medications</h5>
+                  )
+                }
+               
+              </Col>
+              </Row>
+            </Row>
+          </div>
+        </div>
+
+
+
+
+
+
       
     </div>
+
+   
+
+
+
+    </>
   )
 }
 
